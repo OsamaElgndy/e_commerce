712e4e012e042736afbeb8d90110dbc69d30dc0d
osama elgendy <osama elgendy.com>
2024-06-24 18:18:21 +0300

    f

--- next_full _ stack/my-strapi-project/config/plugins.js
+++ next_full _ stack/my-strapi-project/config/plugins.js
@@ -1,1 +1,19 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+   // ...
+   upload: {
+     config: {
+       provider: 'cloudinary',
+       providerOptions: {
+         cloud_name: env('ddzvwalpm'),
+         api_key: env('939213241771858'),
+         api_secret: env('btvu1U0F2sxP9VIBlr8haVPXCH8'),
+       },
+       actionOptions: {
+         upload: {},
+         uploadStream: {},
+         delete: {},
+       },
+     },
+   },
+   // ...
+ });
